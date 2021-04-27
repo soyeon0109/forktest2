f3d5807aa2e150e4e08f5582156c1b01c3b670df
soyeon0109 <3937893@naver.com>
2021-04-27 14:15:07 +0900

    first commit(forktest2 add)

---
+++ githubforktest2/src/main/java/com/multi/githubforktest2/HelloController.java
@@ -0,0 +1,12 @@
+package com.multi.githubforktest2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+@RequestMapping("/hello")
+public String start() {
+	return "hello";
+}
+}
